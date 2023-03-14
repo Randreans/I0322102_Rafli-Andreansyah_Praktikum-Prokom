e4e694ac5c177f0b83c8d2632753e3ca174cecad
Randreans <rafliandreansyah@student.uns.ac.id>
2023-03-14 11:59:42 +0700

    Exercise-01-1stcommit

---
+++ Modul_01/Exercise-01.py
@@ -0,0 +1,5 @@
+nama = "Rafli Andreansyah"
+nim = "I0322102"
+print("Nama =", nama)
+print("NIM  =", nim )
+print("Saya berasal dari Kabupaten Klaten dan bertempat tinggal di Dukuh Bendo, Desa Daleman, Kecamatan Tulung. Kampung halaman saya tersebut merupakan kawasan industri rumah tangga pengolahan pati aren dan mie soon, sehingga sedikit ramai karena banyak orang yang datang dari desa lain untuk bekerja. Meskipun terdapat polusi akibat industri tersebut, tapi masih banyak sawah dan pohon yang membuat udara cukup segar terutama di pagi hari. Selain itu juga terdapat banyak umbul atau kolam mata air yang dapat digunakan untuk berenang dan sebagai sumber air bagi masyarakat. Harapan saya setelah lulus dari Teknik Industri UNS adalah saya dapat menerapkan semua ilmu yang saya dapat dalam dunia kerja dan di kehidupan bermasyarakat mengenai perindustrian.")
